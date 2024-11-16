8f39b5d22b81d284aa10e19e78bed31c14b3cd13
nahumsa <nahumsaa@gmail.com>
2024-11-16 00:01:39 -0300

    ref: split functions for testing

--- dags/brewery.py
+++ dags/brewery.py
@@ -1,21 +1,142 @@
 from __future__ import annotations
+
 import json
 import os
+from datetime import timedelta
+from typing import Optional
 
+import duckdb
 import pendulum
 import requests
-import duckdb
-
 from airflow import DAG
 from airflow.datasets import Dataset
+from airflow.datasets.metadata import Metadata
 from airflow.decorators import task
+from pydantic import BaseModel, TypeAdapter
+
+
+class APIMetadata(BaseModel):
+    total: int
+    page: int
+    per_page: int
+
+
+class BreweryData(BaseModel):
+    id: str
+    name: str
+    brewery_type: str
+    city: str
+    state_province: str
+    postal_code: str
+    country: str
+    state: str
+    address_1: Optional[str]
+    address_2: Optional[str]
+    address_3: Optional[str]
+    longitude: Optional[float]
+    latitude: Optional[float]
+    phone: Optional[str]
+    website_url: Optional[str]
+    street: Optional[str]
+
+
+def get_api_metadata() -> APIMetadata:
+    """Fetches metadata from the openbrewerydb API and converts to a pydantic
+    model.
+
+    Returns:
+        APIMetadata: Information regarding the number of entries.
+    """
+    url = "https://api.openbrewerydb.org/v1/breweries/meta"
+    return APIMetadata(**requests.request("GET", url).json())
+
+
+def get_api_data(page: int, per_page: int = 50) -> list[BreweryData]:
+    """Fetches data from the openbrewerydb API and converts to a pydantic
+    model.
+
+    Args:
+        page(int): Page to get data.
+        per_page(int): Number of entries per page. Defaults to `50`.
+
+    Returns:
+        list[BreweryData]: List of entries from the API.
+    """
+    url = "https://api.openbrewerydb.org/v1/breweries"
+    response = requests.request("GET", url, params={"per_page": per_page, "page": page})
+    return TypeAdapter(list[BreweryData]).validate_python(response.json())
+
+
+def save_json(filepath: str, data: list[dict] | dict):
+    """Save a json to a given filepath.
+
+    Args:
+        filepath(str): path to save the file.
+        data(list[dict] | dict): data to save to a JSON file.
+    """
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+
+    with open(filepath, "w") as file:
+        file.write(json.dumps(data))
+
+
+def generate_aggregate_query(
+    read_filepath: str, read_method: str = "read_parquet"
+) -> str:
+    """Query to create the aggregation of the quantity
+    of brewery types and location using DuckDB.
+
+    Args:
+        read_filepath(str): file path for reading the data.
+        read_method(str): Method to read the file. Defaults to `read_parquet`.
+
+    Returns:
+        str: generated query.
+    """
+    return f"""
+        SELECT 
+            brewery_type
+            , country
+            , state
+            , city
+            , COUNT(*)
+        FROM {read_method}('{read_filepath}')
+        GROUP BY
+            brewery_type
+            , country
+            , state
+            , city
+        """
+
+
+def generate_transform_query(
+    read_filepath: str, read_method: str = "read_json_auto"
+) -> str:
+    """Query to transform JSON data using DuckDB.
+
+    Args:
+        read_filepath(str): file path for reading the data.
+        read_method(str): Method to read the file. Defaults to `read_json_auto`.
+
+    Returns:
+        str: generated query.
+    """
+    return f"""
+        SELECT *
+        FROM {read_method}('{read_filepath}')
+        """
+
 
 with DAG(
-    dag_id="brewery",
+    dag_id="brewery_dataset",
     start_date=pendulum.datetime(2024, 1, 1, tz="UTC"),
     schedule_interval="0 0 * * *",
+    default_args={
+        "retries": 3,
+        "retry_delay": timedelta(minutes=5),
+    },
     catchup=False,
-    tags=["producer", "dataset"],
+    tags=["producer", "dataset", "brewery", "analytics"],
 ):
     raw_dataset = Dataset("/opt/airflow/dags/files/bronze/breweries.json")
     silver_dataset = Dataset("/opt/airflow/dags/files/silver/breweries")
@@ -24,46 +145,38 @@ with DAG(
     )
 
     @task(outlets=[raw_dataset])
-    def fetch_api():
-        os.makedirs(os.path.dirname(raw_dataset.uri), exist_ok=True)
-
-        url = "https://api.openbrewerydb.org/v1/breweries/meta"
-        metadata = requests.request("GET", url).json()
+    def save_bronze():
+        metadata = get_api_metadata()
 
         data = []
 
-        for page in range(int(metadata["total"]) // int(metadata["per_page"]) + 1):
-            url = "https://api.openbrewerydb.org/v1/breweries?per_page=50"
-            response = requests.request(
-                "GET", url, params={"per_page": 50, "page": page}
-            )
-            data.extend(response.json())
+        for page in range(1, metadata.total // metadata.per_page + 2):
+            data.extend(get_api_data(page=page, per_page=metadata.per_page))
 
-        with open(raw_dataset.uri, "w") as file:
-            file.write(json.dumps(data))
+        assert len(data) == metadata.total, "Unable do fetch all data"
+
+        save_json(raw_dataset.uri, [entry.model_dump() for entry in data])
+        yield Metadata(raw_dataset, {"row_count": len(data)})
 
     @task(inlets=[raw_dataset], outlets=[silver_dataset])
-    def save_silver(*, inlet_events):
+    def save_silver():
         os.makedirs(os.path.dirname(silver_dataset.uri), exist_ok=True)
         con = duckdb.connect(database=":memory:")
-        events = inlet_events[raw_dataset]
-        for e in events:
-            print(e)
 
-        con.execute(
-            f"""
-            COPY 
+        transform_query = generate_transform_query(raw_dataset.uri)
+        partition_query = f"""
+            COPY
             (
-                SELECT * 
-                FROM read_json_auto('{raw_dataset.uri}')
-            ) TO '{silver_dataset.uri}' 
+                {transform_query}
+            ) TO '{silver_dataset.uri}'
             (
                 FORMAT PARQUET,
                 PARTITION_BY (country, state, city),
                 OVERWRITE_OR_IGNORE
             );
             """
-        )
+
+        con.execute(partition_query)
 
         con.close()
 
@@ -71,21 +184,9 @@ with DAG(
     def save_gold():
         os.makedirs(os.path.dirname(gold_dataset.uri), exist_ok=True)
         con = duckdb.connect(database=":memory:")
-        aggregate_query = f"""
-        SELECT 
-            brewery_type
-            , country
-            , state
-            , city
-            , COUNT(*)
-        FROM read_parquet('{silver_dataset.uri}/**/*.parquet')
-        GROUP BY 
-            brewery_type
-            , country
-            , state
-            , city
-        """
-        con.execute(aggregate_query)
+        aggregate_query = generate_aggregate_query(
+            read_filepath=f"{silver_dataset.uri}/**/*.parquet"
+        )
 
         con.execute(
             f"COPY ({aggregate_query}) TO '{gold_dataset.uri}' (FORMAT PARQUET);"
@@ -93,4 +194,4 @@ with DAG(
 
         con.close()
 
-    fetch_api() >> save_silver() >> save_gold()
+    save_bronze() >> save_silver() >> save_gold()  # type: ignore
