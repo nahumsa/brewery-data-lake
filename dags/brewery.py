23dada52ae080c33e216fe93adbb6cae35188090
nahumsa <nahumsaa@gmail.com>
2024-11-17 01:12:11 -0300

    feat: add transformation for the silver layer

--- dags/brewery.py
+++ dags/brewery.py
@@ -128,7 +128,22 @@ def generate_transform_query(
         str: generated query.
     """
     return f"""
-        SELECT *
+        SELECT
+            id
+            , name
+            , address_1
+            , address_2
+            , address_3
+            , LOWER(brewery_type) AS brewery_type
+            , LOWER(city) AS city
+            , LOWER(state_province) AS state_province
+            , LOWER(state) AS state
+            , LOWER(country) AS contry
+            , postal_code
+            , CAST(longitude AS FLOAT) AS longitude
+            , CAST(latitude AS FLOAT) AS latitude
+            , phone
+            , website_url
         FROM {read_method}('{read_filepath}')
         """
 
